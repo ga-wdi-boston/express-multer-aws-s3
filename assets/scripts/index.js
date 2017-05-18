4493a506d5eb17b37d97a8fff81fee91d8c49c3d
payne-chris-r <06cpayne@gmail.com>
2017-05-18 12:40:34 -0400

    Sets up with starter code/files
    Wasted a ton of time in `lm01` with the client side doing things
    they've done a million times, but typos and syntax errors cost us
    dearly. To prevent that, I've added all the stuff they've done multiple
    times already, and only have the crux of the lesson left to fill in.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,8 +3,12 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 
+const uploadEvents = require('./uploads/events')
+
 $(() => {
   setAPIOrigin(location, config)
+  // $('#application-x-www-form-data').on('submit', uploadEvents.createUploadEncoded)
+  // $('#multipart-form-data').on('submit', uploadEvents.createUploadMultiPart)
 })
 
 // use require with a reference to bundle the file and use it in this file
